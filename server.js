c55e137fecb3d2d35b004898aade57f811b765e9
Matt Illies <mattillies19@gmail.com>
2019-08-19 20:21:14 -0500

    Added some endpoints to server

---
+++ server.js
@@ -0,0 +1,83 @@
+const express = require('express');
+
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'john',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            joined: new Date(),
+            entries: 0
+        },
+        {
+            id: '124',
+            name: 'sally',
+            email: 'sally@gmail.com',
+            password: 'cakes',
+            joined: new Date(),
+            entries: 0
+        }
+    ]
+}
+
+app.get('/', (req, res) => {
+    res.send(database.users);
+})
+
+app.post('/signin', (req, res) => {
+    if (req.body.email === database.users[0].email && 
+        req.body.password === database.users[0].password) {
+            res.json('succes');
+        } else {
+            res.status(400).json('error logging in');
+        }
+})
+
+app.post('/register', (req, res) => {
+    const { email, name, password } = req.body
+    database.users.push({            
+    id: '125',
+    name: name,
+    email: email,
+    password: password,
+    joined: new Date(),
+    entries: 0
+  })
+  res.json(database.users[database.users.length-1]);
+})
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    const found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found === true
+          return res.json(user);
+        }
+    })
+    if (!found) {
+        res.status(404).json("no such user");
+    }
+})
+
+app.delete('/delete/:id', (req, res) => {
+    const { id } = req.params;
+})
+
+app.listen(3000, () => {
+    console.log("App running on port 3000");
+})
+
+/*
+/ --> res = this is working
+/signin -->POST = succes/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/delete/:userId success/fail
+*/
